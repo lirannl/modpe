eba918adec3a090227c87152ac50174e6dc03152
Liran <iliranisrael@gmail.com>
2013-11-26 19:38:53 +0200

    Update craftingAPI.js

--- craftingAPI.js
+++ craftingAPI.js
@@ -1,12 +1,28 @@
-var GUI;
-var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
 var CTID = 176
 var ironCost = 4
-function craft(input, iAmount, output, oAmount)
+var CTableInv = new Array();
+var CTableInvSlot = new Array();
+function addToCTable()
 {
- 
- 
-} 
+ var CTableInput = new Array();
+ ModPE.readData([useItem.x,useItem.y,useItem.z])
+ if (Player.getCarriedItem != 0)
+ {
+  CTableInput = [Player.getCarriedItem, Player.getCarriedItemData, Player.getCarriedItemCount]
+  if ([CTableInput[0], CTableInput[1]] = [(for (var i=0; i < CTableInv.length; i++) CTableInput[0]),(for (var i=0; i < CTableInv.length; i++) CTableInput[1])])
+  {
+  CTableInv[i[2]] = CTableInv[i[2]] + CTableInput[2]
+  }
+  else {
+   CTableInv.push(CTableInput)
+  }
+  ModPE.saveData([useItem.x,useItem.y,useItem.z], CTableInv)
+ }
+}
+function craft()
+ {
+  
+ }
 function useItem(x,y,z,itemId,blockId,side)
  {
   if (blockId == 54 && side == 1)
@@ -26,7 +42,11 @@ function useItem(x,y,z,itemId,blockId,side)
      }
     }
    }
-   if (blockId == CTID)
+   if (blockId == CTID && ((Level.getTile(x + 1, y, z) == 58) || (Level.getTile(x - 1, y, z) == 58)|| (Level.getTile(x, y, z + 1) == 58)||(Level.getTile(x, y, z - 1) == 58)))
+   {
+    addToCTable()
+   }
+   if (blockId == 58 && ((Level.getTile(x + 1, y, z) == 58) || (Level.getTile(x - 1, y, z) == 58)|| (Level.getTile(x, y, z + 1) == 58)||(Level.getTile(x, y, z - 1) == 58)))
    {
     craft()
    }
