066d66f0add3bef0a2d95003e8dd5dce8494b7e1
Liran <iliranisrael@gmail.com>
2013-11-26 16:28:56 +0200

    Update CraftingBlocks.js

--- CraftingBlocks.js
+++ CraftingBlocks.js
@@ -1,11 +1,11 @@
 var initialized = false;
-var BLOCK_STAINED_CLAY = 187;
-var colour = 0xf0f0f0
- function selevtLevelHook()
- {
-  if (initialized) return;
-   Block.defineBlock(BLOCK_STAINED_CLAY, "Crafting table", [6, [2, 11], 4, 4, 4, 4], 5)
-   Block.setColor(BLOCK_STAINED_CLAY, colour);
-   Block.setDestroyTime(BLOCK_STAINED_CLAY, 3000)
-   Block.setExplosionResistance(BLOCK_STAINED_CLAY, 7)
-  }
+
+function selectLevelHook() { //This is called before the first world is loaded
+        if (!initialized) { //if we have not set up our custom block
+                // define a block with ID 176, name "Crafting table", and the following textures
+                // in the order (bottom, top, south, north, west, east).
+                // in this case, the side textures are the same, and the top/bottom textures are different.
+                Block.defineBlock(176, "Crafting table", [[0, 6], [2, 11], [0, 4], [0, 4], [0, 4], [0, 4]], 5);
+                initialized = true;
+        }
+}
