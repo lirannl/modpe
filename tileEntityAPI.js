d42e17422c9365510dbf113a292c84ab4967147f
Liran <iliranisrael@gmail.com>
2013-11-28 08:42:55 +0200

    Update tileEntityAPI.js

--- tileEntityAPI.js
+++ tileEntityAPI.js
@@ -1,14 +1,13 @@
 //enable retreival and extraction features
 var enableInvMng = true
-//reading the damage value of a tile is not implemented yet, but I have this value as a dummy for now.
-var dmgVal;
 var tileEntity = new Object();
 function useItem( x, y, z, itemId, blockId, side )
  { 
  //add more properties here if needed
- tileEntity = ModPE.readData( [x , y, z, blockId] )
+ level.getData( x, y, z )
+ tileEntity = ModPE.readData( [x , y, z, blockId, dmgVal] )
  tileEntity.location = [ x, y, z ]
- tileEntity.tile = [ blockId, dmgVal ]
+ tileEntity.tile = [ blockId, blockDmg ]
  tileEntity.inv = new Array()
  ModPE.saveData( x, y, z, blockId, tileEntity )
  //Inventory management starts here
