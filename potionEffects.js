8200850d7d9067b4603e144e6460805e669a2cd1
Liran <iliranisrael@gmail.com>
2013-12-18 21:34:49 +0200

    Update potionEffects.js

--- potionEffects.js
+++ potionEffects.js
@@ -1,19 +1,17 @@
-var effectType
-var effectDuration
-var effectAmp
+var effect[]
 var second = 0
 function modTick()
 {
   if (second == 1 && effect[1] == "poison")
   {
-   if (effectDuration > 0 && effectAmp > 0)
+   if (effect[2] > 0 && effect[3] > 0)
    {
-    effectDuration += -1
-    if (Entity.getHealth(getPlayerEnt()) > effectAmp)
+    effect[2] += -1
+    if (Entity.getHealth(getPlayerEnt()) > effect[3])
     {
-     Entity.setHealth( Entity.getHealth(getPlayerEnt()) - effectAmp)
+     Entity.setHealth( Entity.getHealth(getPlayerEnt()) - effect[3])
     }
-    if (Entity.getHealth(getPlayerEnt()) < effectAmp)
+    if (Entity.getHealth(getPlayerEnt()) < effect[3])
     {
      Entity.setHealth(getPlayerEnt(), 1)
     }
@@ -32,9 +30,6 @@ function procCmd(cmd)
 {
   if (cmd.substring(0,6) == "/effect")
   {
-   //setting effect[1] to the text between the space after /effect to the last letter before the next space
-   effectType = cmd.substring(8, cmd.substring(8,cmd.length()).search(" ") -1))
-   effectDuration = Number(cmd.substring(9 + effectType.length(), 9 + cmd.substring(9 + effectType.length(), cmd.length()).search(" ") - 1))
-   effectAmp = Number(cmd.substring(10 + effectType.length() + effectDuration.length(), cmd.substring(10 + effectType.length() + effectDuration.length(), cmd.length()).search(" ") - 1))
+   //extracting the parameters from the command
   }
 }
