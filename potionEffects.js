8191c6a8887954a2a9d0aaa29d4a5ce34168cd4a
Liran <iliranisrael@gmail.com>
2013-12-18 21:19:14 +0200

    Update potionEffects.js

--- potionEffects.js
+++ potionEffects.js
@@ -33,8 +33,8 @@ function procCmd(cmd)
   if (cmd.substring(0,6) == "/effect")
   {
    //setting effect[1] to the text between the space after /effect to the last letter before the next space
-   effectType = cmd.substring(8, cmd.substring(8,cmd.length).search(" ") -1))
-   effectDuration = Number(cmd.substring(9 + effectType.length, 9 + cmd.substring(9 + effectType.length, cmd.length).search(" ") - 1))
-   effectAmp = Number(cmd.substring(10 + effectType.length + effectDuration.length, cmd.substring(10 + effectType.length + effectDuration.length, cmd.length).search(" ") - 1))
+   effectType = cmd.substring(8, cmd.substring(8,cmd.length()).search(" ") -1))
+   effectDuration = Number(cmd.substring(9 + effectType.length(), 9 + cmd.substring(9 + effectType.length(), cmd.length()).search(" ") - 1))
+   effectAmp = Number(cmd.substring(10 + effectType.length() + effectDuration.length(), cmd.substring(10 + effectType.length() + effectDuration.length(), cmd.length()).search(" ") - 1))
   }
 }
