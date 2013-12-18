fb97c345065e1334a357f43bd72338cd8ed046fc
Liran <iliranisrael@gmail.com>
2013-12-18 17:21:23 +0200

    Update potionEffects.js

--- potionEffects.js
+++ potionEffects.js
@@ -1,4 +1,5 @@
 var effect = new array()
+var duration
 //name of effect - change this
 
 function modTick()
@@ -8,4 +9,12 @@ function modTick()
     
   }
 }
-function 
+function procCmd(cmd);
+{
+  if cmd.substring(0,5) == "/effect"
+  {
+   effect[1] = cmd.substring(7, Number(cmd.substring(7,cmd.length).search(" ")))
+   effect[2] = Number(cmd.substring(8 + effect[1].length, cmd.substring(8 + effect[1].length).search(" ") + 1))
+   effect[3] = Number(cmd.substring())
+  }
+}
